98519f86e6415ea3b4a24e1a40ff8830426bec55
Lais Victoria <144797933+laicts@users.noreply.github.com>
2024-04-11 18:16:22 -0300

    Update admin.py

--- app_users/admin.py
+++ app_users/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
+from .models import Reader, Author, Editor
 
 # Register your models here.
+
+@admin.register(Reader) 
+class adminUsers(admin.ModelAdmin):
+    list_display = ['username', 'name', 'email', 'register_date', 'birth_date']
+    search_fields = ['username', 'name']
+    list_filter = ['register_date']
+
