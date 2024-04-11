ea2ce959496ff16769f03572354801a1a9827329
Lais Victoria <144797933+laicts@users.noreply.github.com>
2024-04-11 18:19:08 -0300

    Update admin.py

--- app_news/admin.py
+++ app_news/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import News
 
 # Register your models here.
+
+@admin.register(News) 
+class adminNews(admin.ModelAdmin):
+    list_display = ['title', 'author', 'publish_date']
+    search_fields = ['title', 'author']
+    list_filter = ['publish_date']
